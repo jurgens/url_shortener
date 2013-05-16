f5ced2386d1069523b99417ccf30118b61562eb8
Yury Omelchuk <jurgen@upscript.com>
2013-05-16 23:04:13 +0300

    toggle password field visibility

--- app/models/url.rb
+++ app/models/url.rb
@@ -4,9 +4,11 @@ class Url < ActiveRecord::Base
   before_save :set_url_short
   has_many :visits
 
-  validates :url_long, :format => { :with =>/htt(p|ps):\/\//, :on => :create}
+  validates :url_long, :format => { :with =>/htt(p|ps):\/\//, :on => :create }
+  validates :password_digest, presence: true, if: lambda{ |e| e.private? }
 
   private
+
   def random_url
   	short =  "#{(('a'..'z').to_a + (1..9).to_a).shuffle[0..5].join}"
   	return short if Url.find_by_url_short(short).nil?
